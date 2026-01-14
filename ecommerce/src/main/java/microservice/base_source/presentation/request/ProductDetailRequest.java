a1c8a020301d2c3007fdc8b53227b9dab3973ee1
hoangvanlongse <hoangvanlong.se@gmail.com>
2026-01-14 09:58:32 +0700

    chore: ignore product detail use-case

--- ecommerce/src/main/java/microservice/base_source/presentation/request/ProductDetailRequest.java
+++ ecommerce/src/main/java/microservice/base_source/presentation/request/ProductDetailRequest.java
@@ -1,44 +1,44 @@
-package microservice.base_source.presentation.request;
+// package microservice.base_source.presentation.request;
 
-import java.math.BigDecimal;
+// import java.math.BigDecimal;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import microservice.base_source.data_access.entity.ProductDetail;
+// import jakarta.validation.constraints.Min;
+// import jakarta.validation.constraints.NotNull;
+// import jakarta.validation.constraints.Size;
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+// import microservice.base_source.data_access.entity.ProductDetail;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class ProductDetailRequest {
-	@Size(max = 1000)
-    private String description;
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class ProductDetailRequest {
+// 	@Size(max = 1000)
+//     private String description;
 
-    private String status;
+//     private String status;
 
-    @Min(0)
-    private Integer quantityAvailable;
+//     @Min(0)
+//     private Integer quantityAvailable;
 
-    @NotNull
-    private BigDecimal price;
+//     @NotNull
+//     private BigDecimal price;
 
-    // id của productGeneral (nullable nếu cần)
-    private Long productGeneralId;
+//     // id của productGeneral (nullable nếu cần)
+//     private Long productGeneralId;
 
-    // id của storage tool (nullable)
-    // private Long locatedInStorageTool;
+//     // id của storage tool (nullable)
+//     // private Long locatedInStorageTool;
 
-    public ProductDetail toEntity() {
-        ProductDetail d = new ProductDetail();
-        d.setDescription(this.description);
-        d.setStatus(this.status);
-        d.setQuantityAvailable(this.quantityAvailable);
-        d.setPrice(this.price);
-        d.setProductGeneralId(this.productGeneralId);
-        // d.setLocatedInStorageTool(this.locatedInStorageTool);
-        return d;
-    }
-}
+//     public ProductDetail toEntity() {
+//         ProductDetail d = new ProductDetail();
+//         d.setDescription(this.description);
+//         d.setStatus(this.status);
+//         d.setQuantityAvailable(this.quantityAvailable);
+//         d.setPrice(this.price);
+//         d.setProductGeneralId(this.productGeneralId);
+//         // d.setLocatedInStorageTool(this.locatedInStorageTool);
+//         return d;
+//     }
+// }
