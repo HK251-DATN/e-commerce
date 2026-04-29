77f6b02097909fd7b40b753e3dffdb5e51c77f3b
hoangvanlongse <hoangvanlong.se@gmail.com>
2026-04-29 09:56:33 +0700

    add: onlyu get public coupon

--- src/main/java/microservice/base_source/persistence/repository/CouponRepository.java
+++ src/main/java/microservice/base_source/persistence/repository/CouponRepository.java
@@ -1,7 +1,12 @@
 package microservice.base_source.persistence.repository;
 
+import org.springframework.data.domain.Pageable;
 import microservice.base_source.domain.entity.Coupon;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+	List<Coupon> findByPublicYn(String publicYn, Pageable pageable);
 }
